ed1027ecc530d10acc5b1adc28ac8ac5dc38c0b7
Sairam Patil <86797514+SP-011@users.noreply.github.com>
2022-11-20 16:09:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const getlocation = () =>{
+    // Get location from the user
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+//             document.getElementById('loc').innerHTML = "Latitude : " + position.coords.latitude + " | Longitude : "+position.coords.longitude;
+        });
+    }
+}
