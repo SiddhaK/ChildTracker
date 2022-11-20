0190e8b490d3e905a2de4efd6b3d6ddc01e81f9b
Sairam Patil <86797514+SP-011@users.noreply.github.com>
2022-11-20 16:14:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const getlocation = () =>{
     // Get location from the user
-    if(navigator.geolocation(){
+    if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
             console.log(position);
 //             document.getElementById('loc').innerHTML = "Latitude : " + position.coords.latitude + " | Longitude : "+position.coords.longitude;
